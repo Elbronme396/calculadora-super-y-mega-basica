dc2353126f7adaeb422ff535edc85ab4d3770427
Elbronme396 <92062368+Elbronme396@users.noreply.github.com>
2022-07-29 19:04:44 -0500

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,25 @@
+let nombre = prompt("¿Cual es tu nombre?");
+
+alert("Hola " + nombre + ", Bienvenido a esta calculadora super y mega basica");
+
+let numero1 = prompt("Escribe tu primera cifra");
+let numero2 = prompt("Escribe tu segunda cifra");
+
+let operacion = prompt("¿Que operacion quieres realizar? sumar, restar, dividir o multiplicar");
+
+if (operacion == "sumar"){
+    let resultado = parseInt(numero1) + parseInt(numero2)
+    alert(`El resultado de ${numero1} + ${numero2} es ` + resultado)
+};
+if (operacion == "restar"){
+   let resultado = parseInt(numero1) - parseInt(numero2)
+    alert(`El resultado de ${numero1} - ${numero2} es ` + resultado)
+};
+if (operacion == "dividir"){
+   let resultado = parseInt(numero1) / parseInt(numero2)
+   alert(`El resultado de ${numero1} / ${numero2} es ` + resultado)
+};
+if (operacion == "multiplicar"){
+    let resultado = parseInt(numero1) * parseInt(numero2)
+    alert(`El resultado de ${numero1} * ${numero2} es ` + resultado)
+};
